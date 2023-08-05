46228e53a4ec6a77f70bc8f3c1de80339280f227
Sophie Mispelter <asmispelter@yahoo.fr>
2023-08-05 11:09:29 +0200

    6. PART FOUR: Modal Content Complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -20,9 +20,41 @@ const Cart = (props) => {
     },
   ];
 
+  const MAIN_LIST = [
+    {
+      id: 1,
+      name: "Délices de la Terre",
+      price: 6.1,
+      amount: "x1",
+    },
+    {
+      id: 2,
+      name: "Curry de Riz et Pois Cassés",
+      price: 7.3,
+      amount: "x1",
+    },
+  ];
+
+  const DESSERT_LIST = [
+    {
+      id: 1,
+      name: "Crumble aux Pommes et aux Noix",
+      price: 3.8,
+      amount: "x1",
+    },
+    {
+      id: 2,
+      name: "Mousse au Chocolat Noir Veloutée",
+      price: 4.5,
+      amount: "x1",
+    },
+  ];
+
   return (
     <Modal onCloseModal={props.onClose}>
       <CartList data={STARTERS_LIST} title="Entrées" />
+      <CartList data={MAIN_LIST} title="Plats" />
+      <CartList data={DESSERT_LIST} title="Desserts" />
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>€0</span>
